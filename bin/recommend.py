af94d8596a8f4920025d8e84cf343859e625a3e7
Brian Hie <blhie1@gmail.com>
2022-09-14 00:22:56 +0000

    add alpha to recommend.py

--- bin/recommend.py
+++ bin/recommend.py
@@ -19,10 +19,9 @@ def parse_args():
     )
     parser.add_argument(
         '--alpha',
-        type=str,
-        default=[ 'esm1b', 'esm1v1', 'esm1v2', 'esm1v3', 'esm1v4', 'esm1v5', ],
-        nargs='+',
-        help='Type of language model (e.g., esm1b, esm1v1)'
+        type=float,
+        default=None
+        help='alpha stringency parameter'
     )
     args = parser.parse_args()
     return args
@@ -34,7 +33,7 @@ if __name__ == '__main__':
         get_model_name(model_name) for model_name in args.model_names
     ]
 
-    mutations_models = reconstruct_multi_models(args.sequence, models, alpha=None)
+    mutations_models = reconstruct_multi_models(args.sequence, models, alpha=args.alpha)
     for k, v in sorted(mutations_models.items(), key=lambda item: -item[1]):
         mut_str = f'{k[1]}{k[0] + 1}{k[2]}'
         print(f'{mut_str}\t{v}')
